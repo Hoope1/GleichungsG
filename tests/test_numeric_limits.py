d625c4baf8aa3858e60ee071b0598a479a32c9f7
Hoope1 <109686102+Hoope1@users.noreply.github.com>
2025-09-11 21:05:27 +0200

    Add tests for numeric limits

---
+++ tests/test_numeric_limits.py
@@ -0,0 +1,23 @@
+import sympy as sp
+from fractions import Fraction
+
+from gleichungs_generator import Equation, numeric_limits_ok, x
+
+
+def make_eq(lhs, rhs, sol=Fraction(0)):
+    return Equation("T", sp.Eq(lhs, rhs, evaluate=False), "", sol)
+
+
+def test_numeric_limits_ok_pass():
+    eq = make_eq(sp.Rational(1, 3) * x, sp.Rational(2, 5), Fraction(10, 3))
+    assert numeric_limits_ok(eq)
+
+
+def test_numeric_limits_lcm_fail():
+    eq = make_eq(sp.Rational(1, 121) * x, 0, Fraction(0))
+    assert not numeric_limits_ok(eq)
+
+
+def test_numeric_limits_coeff_fail():
+    eq = make_eq(121 * x + 1, 0, Fraction(-1, 121))
+    assert not numeric_limits_ok(eq)
